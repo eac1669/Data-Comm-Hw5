fb707ca097485513cca38298ab72a1db36fd82ba
eac1669 <124196277+eac1669@users.noreply.github.com>
2026-04-25 11:41:48 -0400

    Finished Task 3

---
+++ docs/source/modules.rst
@@ -0,0 +1,7 @@
+Code Documentation
+==================
+
+.. automodule:: layer3_network_code
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- layer3_network_code.py
+++ layer3_network_code.py
@@ -1,6 +1,6 @@
 """
-Layer 3 Network Topology for Mininet - PART 1 (Fixed version)
-Uses switches for LANs. No routing between LANs.
+Layer 3 Network Topology for Mininet - CLEAN /24 BACKBONE VERSION
+Uses a single shared backbone switch for router connectivity.
 """
 
 from mininet.net import Mininet
@@ -11,23 +11,36 @@ from mininet.link import TCLink
 
 
 class Router(Node):
-    """Router with IP forwarding disabled."""
     def config(self, **params):
         super(Router, self).config(**params)
+        self.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    def terminate(self):
         self.cmd('sysctl -w net.ipv4.ip_forward=0')
+        super(Router, self).terminate()
 
-
+"""
+Intention:
+Build and start a Layer 3 Mininet topology with three routers, three LANs,
+and a shared backbone switch. Assign IP addresses, configure routing,
+and verify connectivity using ping tests.
+"""
 def create_network():
     net = Mininet(link=TCLink)
 
+    # Routers
     rA = net.addHost('rA', cls=Router)
     rB = net.addHost('rB', cls=Router)
     rC = net.addHost('rC', cls=Router)
 
+    # Switches
     s1 = net.addSwitch('s1', failMode='standalone')
     s2 = net.addSwitch('s2', failMode='standalone')
     s3 = net.addSwitch('s3', failMode='standalone')
 
+    s0 = net.addSwitch('s0', failMode='standalone')
+
+    # Hosts
     hA1 = net.addHost('hA1', ip='20.10.172.131/26')
     hA2 = net.addHost('hA2', ip='20.10.172.132/26')
 
@@ -37,6 +50,7 @@ def create_network():
     hC1 = net.addHost('hC1', ip='20.10.172.193/27')
     hC2 = net.addHost('hC2', ip='20.10.172.194/27')
 
+    # LAN LINKS
     net.addLink(hA1, s1)
     net.addLink(hA2, s1)
     net.addLink(rA, s1)
@@ -49,37 +63,42 @@ def create_network():
     net.addLink(hC2, s3)
     net.addLink(rC, s3)
 
-    net.addLink(rA, rB)
-    net.addLink(rB, rC)
-    net.addLink(rA, rC)
+    net.addLink(rA, s0)
+    net.addLink(rB, s0)
+    net.addLink(rC, s0)
 
     net.start()
 
+    # LAN interfaces
     rA.setIP('20.10.172.129/26', intf='rA-eth0')
     rB.setIP('20.10.172.1/25', intf='rB-eth0')
     rC.setIP('20.10.172.195/27', intf='rC-eth0')
 
     rA.setIP('20.10.100.1/24', intf='rA-eth1')
-    rA.setIP('20.10.100.2/24', intf='rA-eth2')
+    rB.setIP('20.10.100.2/24', intf='rB-eth1')
+    rC.setIP('20.10.100.3/24', intf='rC-eth1')
 
-    rB.setIP('20.10.100.3/24', intf='rB-eth1')
-    rB.setIP('20.10.100.4/24', intf='rB-eth2')
+    # Host default routes
+    hA1.cmd('ip route add default via 20.10.172.129')
+    hA2.cmd('ip route add default via 20.10.172.129')
 
-    rC.setIP('20.10.100.5/24', intf='rC-eth1')
-    rC.setIP('20.10.100.6/24', intf='rC-eth2')
+    hB1.cmd('ip route add default via 20.10.172.1')
+    hB2.cmd('ip route add default via 20.10.172.1')
 
-    print("\nTesting LAN connectivity:\n")
+    hC1.cmd('ip route add default via 20.10.172.195')
+    hC2.cmd('ip route add default via 20.10.172.195')
 
-    print("LAN A:")
+
+    print("\nLAN A:")
     print(net.ping([hA1, hA2]))
 
-    print("LAN B:")
+    print("\nLAN B:")
     print(net.ping([hB1, hB2]))
 
-    print("LAN C:")
+    print("\nLAN C:")
     print(net.ping([hC1, hC2]))
 
-    print("\nCross-LAN test (shouldn't work in Task 2):\n")
+    print("\nCross LAN test:")
     print(net.pingAll())
 
     CLI(net)
